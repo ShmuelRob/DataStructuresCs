b6c6dd7aa11f47efdd52cd641ffaa1aa7b8171a1
ShmuelRob <shmuel.robinov@gmail.com>
2022-08-01 20:00:54 +0300

    Implements the Stretegy pattern on Stack and Queue

---
+++ DSTests/BstTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructuresCs;
+using System;
+
+namespace DSTests
+{
+    [TestClass]
+    public class BstTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
---
+++ DataStructuresCs/BST/BstNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCs.BST
+{
+    internal class BstNode<T>
+    {
+        internal T value;
+        internal BstNode<T> left;
+        internal BstNode<T> right;
+        internal BstNode(T val)
+        {
+            value = val;
+            left = right = null;
+        }
+        public bool IsLeaf => left == null && right == null;
+        public override string ToString() => value.ToString();
+    }
+}
