be61a628be9c471d6eb3865edc7e762e0b2e28de
ShmuelRob <shmuel.robinov@gmail.com>
2022-08-17 23:05:10 +0300

    add the hash table files

---
+++ DataStructuresCs/HashTable.cs
@@ -0,0 +1,43 @@
+﻿using DataStructuresCs.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCs
+{
+    public class HashTable<TKey, TValue>
+    {
+        LinkedList<HashData<TKey, TValue>>[] hashArray;
+        public int itemsCnt;
+
+        public HashTable(int capacity)
+        {
+            hashArray = new LinkedList<HashData<TKey, TValue>>[capacity];
+            itemsCnt = 0;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            int index = HashFunc(key);
+            var d = new HashData<TKey, TValue>(key, value);
+            if (hashArray[index] == null)
+            {
+                hashArray[index] = new LinkedList<HashData<TKey, TValue>>();
+            }
+            else
+            {
+                foreach (var data in hashArray[index])
+                {
+                    if (data.key.Equals(key)) throw new ArgumentException("An item with the same key has already been added");
+                }
+            }
+            hashArray[index].AddFirst(d);
+            itemsCnt++;
+        }
+
+        private int HashFunc(TKey key) =>
+            Math.Abs(key.GetHashCode()) % hashArray.Length;
+    }
+}
---
+++ DataStructuresCs/Nodes/HashData.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructuresCs.Nodes
+{
+    internal class HashData<TKey, TValue>
+    {
+        public TKey key;
+        public TValue value;
+
+        public HashData(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
